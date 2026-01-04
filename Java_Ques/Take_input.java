2f3de73a95b94a09772921b7bdaf727deecc6736
Meetkatlana <meetkatlana7@gmail.com>
2026-01-04 23:42:54 +0530

    Newadd

---
+++ Java_Ques/Take_input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Take_input {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Name :");
+    String str = sc.nextLine();
+    System.out.println("Hello, How are you ?? " + str);
+}
+}
