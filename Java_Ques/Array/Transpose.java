e36f878ca86a77befb9d448c9298a213c0c8fd3b
Meetkatlana <meetkatlana7@gmail.com>
2026-01-16 15:01:01 +0530

    m

---
+++ Java_Ques/Array/Transpose.java
@@ -0,0 +1,19 @@
+
+public class Transpose {
+    public static void main(String[] args) {
+        
+    }
+    static int[][] transpose(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int arr[][]=new int[col][row];
+        for(int i=0;i<col;i++)
+        {
+            for(int j=0;j<row;j++)
+            {
+            arr[i][j]=matrix[j][i];
+            }
+        }
+        return arr;
+    }
+}
